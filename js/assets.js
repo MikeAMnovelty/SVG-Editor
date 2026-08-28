aaf7a66cf6a3c64ba028357c8f0e29e7045850d1
MikeAMnovelty <mike@amnovelty.com>
2026-08-27 19:38:40 -0500

    Add asset metadata for SVG and preview images

---
+++ js/assets.js
@@ -0,0 +1,20 @@
+[
+  {
+    "id": "anchor",
+    "name": "Anchor",
+    "file": "./assets/svg/anchor.svg",
+    "preview": "./assets/previews/anchor.png"
+  },
+  {
+    "id": "wave",
+    "name": "Wave",
+    "file": "./assets/svg/wave.svg",
+    "preview": "./assets/previews/wave.png"
+  },
+  {
+    "id": "monogram",
+    "name": "Monogram",
+    "file": "./assets/svg/monogram.svg",
+    "preview": "./assets/previews/monogram.png"
+  }
+]
